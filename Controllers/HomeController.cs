2c8c0d2087bf106de73948c6d45a618bcd681732
heinwaihtet <heinwaihtet@myanmardcr.com>
2022-03-14 22:34:24 +0630

    1. add static file middle ware 2. add mvc middle ware and configure mvc service 3. add developer exception middle ware 4. configure wwwroot and controllers folder

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.netCore_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "This is MVC Controller";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,35 @@
 var builder = WebApplication.CreateBuilder(args);
+
+/** 
+ * Add services to the container.
+ */
+/** Add MVC service */
+builder.Services.AddMvc(options =>
+{
+    /** check end point routing */
+    options.EnableEndpointRouting = false;
+});
+
+
+/**
+ * Configure the HTTP request pipeline.
+ */
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+/** Check Development mode and add Developer Exception */
+if (app.Environment.IsDevelopment())
+{
+    DeveloperExceptionPageOptions options = new DeveloperExceptionPageOptions();
+    options.SourceCodeLineCount = 10;
+    app.UseDeveloperExceptionPage(options);
+}
+
+/** add static file middleware for connect under wwwroot */
+app.UseStaticFiles();
+
+/** Configure MVC controller with default routes */
+app.UseMvcWithDefaultRoute();
 
 app.Run();
