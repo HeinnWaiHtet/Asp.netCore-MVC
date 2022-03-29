cb9efc15eaab24800a913b4e826dfe299ee3a632
heinwaihtet <heinwaihtet@myanmardcr.com>
2022-03-29 17:47:46 +0630

    request URL Not Found Error Handling Using UseStatusCodePagesWithReExecute MiddleWare

--- Controllers/ErrorController.cs
+++ Controllers/ErrorController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Asp.netCore_MVC.Controllers
 {
@@ -12,10 +13,13 @@ namespace Asp.netCore_MVC.Controllers
         [Route("/Error/{statusCode}")]
         public IActionResult HttpStatusCodeHandler(int statusCode)
         {
+            var statusCodeResult = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
             switch (statusCode)
             {
                 case 404:
                     ViewBag.ErrorMessage = "Sorry, the resource you requested cannot be found";
+                    ViewBag.Path = statusCodeResult?.OriginalPath;
+                    ViewBag.QS = statusCodeResult?.OriginalQueryString; ;
                     break;
             }
             return View("NotFound");
--- Program.cs
+++ Program.cs
@@ -34,7 +34,7 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    app.UseStatusCodePagesWithRedirects("/Error/{0}"); // Default Status Code Error Page
+    app.UseStatusCodePagesWithReExecute("/Error/{0}"); // Default Status Code Error Page
 }
 
 /** add static file middleware for connect under wwwroot */
