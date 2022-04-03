5f1bb929672742d1054a52ebe583c867e9c4b0e7
heinwaihtet <heinwaihtet@myanmardcr.com>
2022-04-03 22:12:15 +0630

    Create Role By Using Roldemanage<IdentiyRole>.createAsync

---
+++ Controllers/AdministrationController.cs
@@ -0,0 +1,71 @@
+﻿using Asp.netCore_MVC.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.netCore_MVC.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        #region Properties
+
+        private readonly RoleManager<IdentityRole> roleManager;
+        #endregion
+
+        #region Constructor
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+        #endregion
+
+        #region CreateRole
+
+        /// <summary>
+        /// Create Role View
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return this.View();
+        }
+
+        /// <summary>
+        /// Create Role By Request ViewModel
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                /** Create IdentiyRole */
+                var identityRole = new IdentityRole()
+                {
+                    Name = model.RoleName
+                };
+
+                /** Create Role By IdentiyRole */
+                var result = await roleManager.CreateAsync(identityRole);
+
+                /** Check Role Create Success or not */
+                if (result.Succeeded)
+                {
+                    return this.RedirectToAction("index", "home");
+                }
+
+                /** Add Error When Role Creation Fail */
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+            }
+
+            return View();
+        }
+        #endregion
+
+    }
+}
---
+++ ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Asp.netCore_MVC.ViewModels
+{
+    public class CreateRoleViewModel
+    {
+        #region Properties
+
+        [Required(ErrorMessage = "Please Enter Role Name")]
+        public string? RoleName { get; set; }
+        #endregion
+    }
+}
