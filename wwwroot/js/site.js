d8e8443d4902413a3abb114b4923f2b39c612ff8
heinwaihtet <heinwaihtet@myanmardcr.com>
2022-04-04 23:49:18 +0630

    delete user confirmation trick

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,18 @@
-﻿console.log('script run');
+﻿
+/**
+ * Delete Confirmation Panel Toggle State
+ * @param {any} uniqueId
+ * @param {any} isDeleteClicked
+ */
+function confirmDelete(uniqueId, isDeleteClicked) {
+    let deleteSpan = 'deleteSpan_' + uniqueId;
+    let confirmDeleteSpan = 'confirmDeleteSpan_' + uniqueId;
+
+    if (isDeleteClicked) {
+        $(`#${deleteSpan}`).hide();
+        $(`#${confirmDeleteSpan}`).show();
+    } else {
+        $(`#${deleteSpan}`).show();
+        $(`#${confirmDeleteSpan}`).hide();
+    }
+}
